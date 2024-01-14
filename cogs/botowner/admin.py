4116e90a50a7c020f4be8d240cf4bfce3bfac5e4
AustrianNoah <soziales@austriannoah.xyz>
2024-01-14 06:55:03 +0100

    schulz

--- cogs/botowner/admin.py
+++ cogs/botowner/admin.py
@@ -10,7 +10,10 @@ class AdminDB(ezcord.DBHandler):
         super().__init__("Admins.db")
 
     async def setupDB(self):
-        await self.execute("CREATE TABLE IF NOT EXISTS admins (id INTEGER PRIMARY KEY, adminname VARCHAR(255))")
+        await self.execute("CREATE TABLE IF NOT EXISTS admins (userid INTEGER PRIMARY KEY, adminname VARCHAR(255))")
+
+    async def add_admin(self, userid: int, adminname: str):
+        await self.execute("INSERT OR IGNORE INTO admins (userid, adminname) VALUES (?, ?)", (userid, adminname))
 
 
 db = AdminDB()
@@ -29,6 +32,15 @@ class Help(commands.Cog):
     @admin.command(name="help")
     async def _help(self, ctx: discord.ApplicationContext):
         await ctx.respond("Admin Help successfully")
+
+    @admin.command(name="add", description="Add a Bot Owner / Admin to the Panel")
+    @commands.has_permissions(administrator=True)
+    async def _add(self, ctx: discord.ApplicationContext, user: Option(discord.Member, "Choose a User", required=True)):
+        try:
+            await db.add_admin(userid=user.id, adminname=user.name)
+            await ctx.respond(f"Added {user.name} to {ctx.guild.name} Admins", ephemeral=True)
+        except:
+            await ctx.respond(f"There was an Error while we trying to add an Admin with the ID(||{user.id}||)", ephemeral=True)
 
 def setup(bot: commands.Bot):
     bot.add_cog(Help(bot))
